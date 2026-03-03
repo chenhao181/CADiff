f4ed1bc2ebd8b16afaff2af9a9e6f0c8613ce503
chenhao181 <ChenHao181@mail.ustc.edu.cn>
2026-03-03 15:47:16 +0800

    update: modify training script for CADiff

--- src/scripts.sh
+++ src/scripts.sh
@@ -2,7 +2,7 @@
 python main.py -d movielens -luser 0.2 -lrec 0.4 -lmse 1.5 -temp 0.6  --warmup 160
 
 # KuaiRec
-python main.py -d kuairec -luser 0.1 -lrec 0.1 -1mse 1.2 -warmup 3
+python main.py -d kuairec -luser 0.1 -lrec 0.1 -lmse 1.2 -warmup 3
 
 # Yelp
 python main.py -d yelp -luser 0.05 -lrec 0.6 -lmse 1.0 -temp1.4 --warmup 190
